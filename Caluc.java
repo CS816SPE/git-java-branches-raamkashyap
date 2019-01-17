28ff6470e9b7fb746db503faf7ed00048b3b7012
raamkashyap <ramkashyap26@gmail.com>
2019-01-17 10:51:32 +0530

    Second Commit

--- Caluc.java
+++ Caluc.java
@@ -6,7 +6,7 @@ public class Caluc{
 		Scanner scan = new Scanner(System.in);
 
 		do{
-			System.out.print("1.ADD TWO NUMBERS\n");
+			System.out.print("1.Substract TWO NUMBERS\n");
 			System.out.print("2. Exit\n\n");
 			System.out.print("Enter Your Choice : ");
 			choice = scan.next().charAt(0);
@@ -14,7 +14,7 @@ public class Caluc{
 				case '1' : System.out.print("Enter Two Number :");
 						   a = scan.nextFloat();
 						   b = scan.nextFloat();
-						   res = a+b;
+						   res = a-b;
 						   System.out.print("Result = " + res);
 						   break;
 				case '2' : System.exit(0);
